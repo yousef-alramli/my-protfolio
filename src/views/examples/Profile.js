8497e17152fe2398c9b71215cf0d8341a8562aea
yousef-alramli <ysabdys1996@gmail.com>
2021-09-11 23:16:49 +0300

    more edits

--- src/views/examples/Profile.js
+++ src/views/examples/Profile.js
@@ -39,29 +39,9 @@ class Profile extends React.Component {
           <section className="section-profile-cover section-shaped my-0">
             {/* Circles background */}
             <div className="shape shape-style-1 shape-default alpha-4">
-              <span />
-              <span />
-              <span />
-              <span />
-              <span />
-              <span />
-              <span />
             </div>
             {/* SVG separator */}
             <div className="separator separator-bottom separator-skew">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                preserveAspectRatio="none"
-                version="1.1"
-                viewBox="0 0 2560 100"
-                x="0"
-                y="0"
-              >
-                <polygon
-                  className="fill-white"
-                  points="2560 0 2560 100 0 100"
-                />
-              </svg>
             </div>
           </section>
           <section className="section">
